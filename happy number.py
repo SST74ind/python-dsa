f6315699c4ccbc299bf0ed7f92596285ae13e94a
Swarnoj Saha Talukdar <sahatalukdarswarnoj@gmail.com>
2026-05-16 16:36:29 +0530

    Update happy number.py

--- happy number.py
+++ happy number.py
@@ -7,12 +7,12 @@ class Solution(object):
                 s+=d*d
                 n//=10
             return s
-        tortoise=n
-        hare=get_next(n)
-        while hare!=1 and tortoise!=hare:
-            tortoise=get_next(tortoise)
-            hare=get_next(get_next(hare))
-        return hare==1
+        slow=n
+        fast=get_next(n)
+        while slow!=1 and slow!=fast:
+            slow=get_next(slow)
+            fast=get_next(get_next(fast))
+        return fast==1
 # Example usage:
 solution = Solution()
 print(solution.isHappy(19))
